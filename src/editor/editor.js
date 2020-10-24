7f3fb7bd0353ca13b9a6667e2af1102ab50aa4ba
sedx876 <swatkins876@gmail.com>
2020-10-24 10:29:14 -0400

    more boilerplate

---
+++ src/editor/editor.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import ReactQuill from 'react-quill';
+import debounce from '../helpers';
+import BorderColorIcon from '@material-ui/icons/BorderColor';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles';
+
+class EditorComponent extends Component {
+
+  constructor(){
+    super();
+  }
+
+  render(){
+    return(
+      <div>Hello from editor</div>
+    )
+  }
+}
+
+export default withStyles(styles)(EditorComponent)
