ae55431f85e3f58fdcf08469c5c96e27336d7268
sedx876 <swatkins876@gmail.com>
2020-10-24 10:14:23 -0400

    componentDidMount in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React, {Component} from 'react'
 import './styles/App.css';
 
+const firebase = require('firebase')
+
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      selectedNoteIndex: null,
+      selectedNote: null,
+      notes: null
+    };
+  }
+
+  componentDidMount = () => {
+    firebase
+      .firestore()
+      .collection('notes')
+      .onSnapshot(serverUpdate => {
+        const notes = serverUpdate.docs.map(_doc => {
+          const data = _doc.data();
+          data['id'] = _doc.id;
+          return data;
+        });
+        console.log(notes);
+        this.setState({ notes: notes });
+      });
+  }
+
   render(){
     return(
       <>
