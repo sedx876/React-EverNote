0ad75039898af0d51c0c2f67622e1c864d634057
sedx876 <swatkins876@gmail.com>
2020-10-24 09:57:25 -0400

    Added helpers.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React, {Component} from 'react'
 import './styles/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render(){
+    return(
+      <>
+      </>
+    )
+  }
+
 }
 
 export default App;
